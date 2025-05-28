582fced0f0bdf148718405a0002d68fd18b43a45
Arpit Jain <arpit.jain@renegadeinsurance.com>
2025-05-28 16:59:44 +0530

    Remove unnecessary files

--- utils.py
+++
@@ -1,9 +0,0 @@
-def make_api_call(url):
-    import requests
-
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an error for bad responses
-    return response.json()
-
-def extract_redirect_url(api_response):
-    return api_response.get('redirect_url')  # Adjust the key based on the actual API response structure
